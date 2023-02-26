cafa5b94fce02886053b53d1872e9da82b9fca5b
NaashNix <thilakshanaravindu@gmail.com>
2023-02-26 11:14:00 +0530

    Main logo added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import FoodTileContainer from './components/FoodTypeTile/FoodTileContainer';
+import MainLogo from './components/NavBar/MainLogo';
 
 
 function App() {
   return (
     <div className="App">
-      <FoodTileContainer />      
+      <MainLogo />
+      <FoodTileContainer />   
+      
     </div>
   );
 }
