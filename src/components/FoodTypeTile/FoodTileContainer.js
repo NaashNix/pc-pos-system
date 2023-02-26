3b574c1a3c4c34da3ae67987b53201ed502fadba
NaashNix <thilakshanaravindu@gmail.com>
2023-02-26 11:00:58 +0530

    Main logo added

--- src/components/FoodTypeTile/FoodTileContainer.js
+++ src/components/FoodTypeTile/FoodTileContainer.js
@@ -7,7 +7,7 @@ const FoodTileContainer = () => {
    return (
       <div className={classes.container} >
          <FoodTypeTile source={img1} heading="Breakfast" defColor="#B4FFF6" onColor="#2FFFE6" />
-         <FoodTypeTile heading="Dinner" defColor="#A4FFB8" onColor="#45FF6E" />
+         <FoodTypeTile source={img1} heading="Dinner" defColor="#A4FFB8" onColor="#45FF6E" />
          <FoodTypeTile heading="Lunch" defColor="#FFF6A4" onColor="#FFEA2C" />
          <FoodTypeTile heading="Drinks" defColor="#9BB7FF" onColor="#2764FF" />
          <FoodTypeTile heading="Desserts" defColor="#FFB4B4" onColor="#FF8282" />
