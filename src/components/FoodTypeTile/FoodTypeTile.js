628b0f2e512b1603bf38c1f0e73135de390ce70a
NaashNix <thilakshanaravindu@gmail.com>
2023-02-26 09:24:32 +0530

    Image sty;ed

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -6,9 +6,9 @@ import breakfast_image from '../../assets/breakfast.png';
 const FoodTypeTile = () => {
    return (
       <div className={classes.outer} >
-         <img src={breakfast_image} alt="" />
+         <img className={classes.image} src={breakfast_image} alt="" />
          <span className={classes.mainTitle} >Breakfast</span>
-         <span>13 Items</span>
+         <span className={classes.subTitle} >13 Items</span>
       </div>
    );
 }
