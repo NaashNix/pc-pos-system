b49739f2e6681d9d9db62687b1939c3a9a4e46da
NaashNix <thilakshanaravindu@gmail.com>
2023-02-26 09:48:00 +0530

    Mouse Events done

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import classes from './FoodTypeTile.module.css'; 
-import breakfast_image from '../../assets/breakfast.png';
-
+import React from "react";
+import classes from "./FoodTypeTile.module.css";
+import breakfast_image from "../../assets/breakfast.png";
 
 const FoodTypeTile = () => {
-   return (
-      <div className={classes.outer} >
-         <img className={classes.image} src={breakfast_image} alt="" />
-         <div className={classes.titleHolder} >
-            <span className={classes.mainTitle} >Breakfast</span>
-            <span className={classes.subTitle} >13 Items</span>
-         </div>
-      </div>
-   );
-}
+	const mouseEnterHandler = () => {
+		console.log("Mouse Enter");
+	};
+
+   const mouseLeaveHandler = () => {
+      console.log('Mouse Leaved');
+   }
+
+	return (
+		<div 
+         className={classes.outer} 
+         onMouseEnter={mouseEnterHandler}
+         onMouseLeave={mouseLeaveHandler}
+      >
+			<img className={classes.image} src={breakfast_image} alt="" />
+			<div className={classes.titleHolder}>
+				<span className={classes.mainTitle}>Breakfast</span>
+				<span className={classes.subTitle}>13 Items</span>
+			</div>
+		</div>
+	);
+};
 
 export default FoodTypeTile;
