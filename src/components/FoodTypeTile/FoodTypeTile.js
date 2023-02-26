d8f75119ce0474aa15cedc8226a2fdefc691413c
NaashNix <thilakshanaravindu@gmail.com>
2023-02-26 09:31:08 +0530

    Image sty;ed

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -7,8 +7,10 @@ const FoodTypeTile = () => {
    return (
       <div className={classes.outer} >
          <img className={classes.image} src={breakfast_image} alt="" />
-         <span className={classes.mainTitle} >Breakfast</span>
-         <span className={classes.subTitle} >13 Items</span>
+         <div className={classes.titleHolder} >
+            <span className={classes.mainTitle} >Breakfast</span>
+            <span className={classes.subTitle} >13 Items</span>
+         </div>
       </div>
    );
 }
