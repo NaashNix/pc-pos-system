3f64ad823864a96d9b9e50beb589a570f9b0ad68
NaashNix <thilakshanaravindu@gmail.com>
2023-02-26 09:18:23 +0530

    Title Styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import FoodTypeTile from './components/FoodTypeTile/FoodTypeTile';
 
--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -5,8 +5,10 @@ import breakfast_image from '../../assets/breakfast.png';
 
 const FoodTypeTile = () => {
    return (
-      <div>
+      <div className={classes.outer} >
          <img src={breakfast_image} alt="" />
+         <span className={classes.mainTitle} >Breakfast</span>
+         <span>13 Items</span>
       </div>
    );
 }
