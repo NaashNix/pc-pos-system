7a3d8609e95715bffbf9c8a35ec498b8243a7dd9
NaashNix <thilakshanaravindu@gmail.com>
2023-03-05 08:57:59 +0530

    Cart Item started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CartItem from './components/Cart/CartItem';
 import FoodTileContainer from './components/FoodTypeTile/FoodTileContainer';
 import ItemTileContainer from './components/ItemTiles/ItemTileContainer';
 import SingleItem from './components/ItemTiles/SingleItem';
@@ -8,14 +9,15 @@ import RecentOrdersContainer from './components/RecentOrders/RecentOrdersContain
 function App() {
   return (
     <div className="App">
-      {/* <UpperNavBar /> */}
+      {/* <UpperNavBar />
       <div className="center" >
         <RecentOrdersContainer />
        <div className="innerCenter" >
           <FoodTileContainer />
           <ItemTileContainer /> 
        </div>
-      </div>
+      </div> */}
+      <CartItem />
     </div>
   );
 }
